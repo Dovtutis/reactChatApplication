1ee28452dfcb60d33ed354eeded1e161104f5e27
Dovtutis <dovtutis@gmail.com>
2021-04-09 10:30:07 +0300

    Log in form and error handling done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 const App = () => {
+    console.log(localStorage.getItem('username'));
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    
     return (
         <ChatEngine 
             height="100vh"
             projectID="c49200d9-bc21-4a5e-a081-b383dab84ca6"
-            userName="Dovtutis"
-            userSecret="memoriam"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     )
